143d169e8648a3d76b1a681b982a45269c5e9e22
SaliSim <salisimiyu319@gmail.com>
2026-01-28 21:15:20 +0300

    creating website infra using terraform

---
+++ provider.tf
@@ -0,0 +1,11 @@
+#GCP provider
+
+provider "google" {
+  project = var.gcp_project
+  region  = var.gcp_region
+}
+
+provider "google-beta" {
+  project = var.gcp_project
+  region  = var.gcp_region
+}
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+gcp_project = "spherical-depth-476520-e2"
+gcp_region  = "us-central1"
